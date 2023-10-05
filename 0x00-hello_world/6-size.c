61b3dfe5e353fc9e2dc2487f8539036e3d2b7131
KatliKoma <katlegokoma1@gmail.com>
2023-10-05 14:28:20 -0700

    ize is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of short: %zu bytes\n", sizeof(short));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long));
 printf("Size of long long int: %zu bytes\n", sizeof(long long));
