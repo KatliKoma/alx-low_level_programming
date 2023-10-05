814436a1d6356732021df929e4c63f9de1cafd97
KatliKoma <katlegokoma1@gmail.com>
2023-10-05 12:32:26 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-
-
+/**
+ * printing messages
+ *
+ */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle\n");
-		getchar();
-		return 0;
+    puts("Programming is like building a multilingual puzzle\n");
+    getchar();
+    return (0);
 }
+
