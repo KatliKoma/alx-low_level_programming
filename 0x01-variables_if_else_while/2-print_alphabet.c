ffa63022696a584e8721b9af999b0d9958a715c0
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 16:34:50 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alph;
