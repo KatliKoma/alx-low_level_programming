0d2e530a081c21f0e546bd75310573127a107c63
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 16:36:18 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game'
+ * Description: 'I play what I call the alphabet game'
  *
  *
  * Return: Always 0 (Success)
