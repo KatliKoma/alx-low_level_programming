9a0eed1a1b77cf7e1e5d9b43f16204c7a5a0f1eb
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 16:45:19 -0700

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'When I was having that alphabet soup, I never thought that it would pay off'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alph;
+
+        for (alph = 'a'; alph >= 'a' && alph <= 'z' ; alph++)
+        {
+		if (alph != 'q' && alph != 'e')
+	       	{
+			putchar(alph);
+		}
+        }
+
+        putchar('\n');
+        return (0);
+}
