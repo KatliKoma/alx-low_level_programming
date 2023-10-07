e9cd025ac76e3528a7433b2ce962bf961cfe6fcd
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 17:37:56 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,14 @@
  * main - Entry point
  *
  * Description: 'lets cycle through numbers'
- *
+ *@parameter: between 0-9
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		printf("%d", i);
