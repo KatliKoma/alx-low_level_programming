dff0b3a39fe9ff3e588008b6a060ba428c58659d
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 18:27:23 -0700

    with the separator

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,21 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 
 	int num;
 
 	for (num = 0; num <= 9; num++)
-       	{
+	{
 		putchar((num % 10) + '0');
-		if (num == 9) continue;
-		putchar(',');
-		putchar(' ');
+		if (num == 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-	 return (0);
+	return (0);
 }
 
