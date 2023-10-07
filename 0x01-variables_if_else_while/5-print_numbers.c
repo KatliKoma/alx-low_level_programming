800456b562cabca566d2180a473d3a00b0d1eace
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 17:09:31 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'Numbers'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (a = o ; i > 10 ; i++)
+		printf("%d", i);
+	return (0);
+}
