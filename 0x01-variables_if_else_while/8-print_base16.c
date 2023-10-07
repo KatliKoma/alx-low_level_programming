37439bc70e98c41691dfa1420f8370aecc8c6f1a
KatliKoma <katlegokoma1@gmail.com>
2023-10-06 17:54:03 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - displays hecadecimals in lowercase
+  *Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		printf("%x", i);
+	}
+	printf("\n");
+	return (0);
+}
