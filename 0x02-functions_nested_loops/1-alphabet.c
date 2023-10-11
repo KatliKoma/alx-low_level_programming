7d340daa3c4cabe199b846514954e2376d955c45
KatliKoma <katlegokoma1@gmail.com>
2023-10-11 01:42:52 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * prints alphabet in lowecase
+ *
+ * no return.
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph >= 'a' && alph <= 'z' ; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 void printMess(void);
-
+void print_alphabet(void);
 #endif
 
