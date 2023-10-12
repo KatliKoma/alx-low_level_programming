976c9fc0a08811aaa174c73cb038e0ea37cd0d4a
KatliKoma <katlegokoma@1gmail.com>
2023-10-12 12:59:51 +0200

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char alph;
-	for (i = i; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		/*cycles throught the loop*/
 		for (alph = 'a'; alph <= 'z'; alph++)
