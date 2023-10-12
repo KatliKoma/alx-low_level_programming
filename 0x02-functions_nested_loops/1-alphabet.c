1f02b8f5fe9159f9c7b0c1a21c29d3e85bfabcea
KatliKoma <katlegokoma@1gmail.com>
2023-10-12 11:46:42 +0200

    insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * prints alphabet in lowecase
- *
- * no return.
- */
+*prints alphabet in lowecase
+*
+*return 0
+*/
+
+/* this function print the alphabet*/
 void print_alphabet(void)
 {
 	char alph;
 
-        for (alph = 'a'; alph <= 'z'; alph++)
-        {
-                putchar(alph);
-        }
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
 	putchar('\n');
-	return;
+	return (0);
 }
