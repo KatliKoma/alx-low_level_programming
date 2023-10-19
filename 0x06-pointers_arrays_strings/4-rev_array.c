f9e3f594d83c2f5824e2778d765e103f4eb997d4
KatliKoma <katlegokoma@1gmail.com>
2023-10-19 23:35:40 +0200

    content of an array of integers.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,36 +2,21 @@
 #include <stdio.h>
 
 /**
- * reverse_array - reverses the content of an array 
+ * reverse_array - reverses the content of an array
+ * print_array - prints content of an array of integers
  * @a: array of integers
- * @n: array size
+ * @n: number of elements of the array
  *
  * Return: no return
  */
-void reverse_array(int *a, int n) 
+void reverse_array(int *a, int n)
 {
 	int i, temp;
-	for (i = 0; i < n / 2; i++) 
+
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
 	}
-}
-
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
 }
