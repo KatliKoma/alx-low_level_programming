edd514266f9fc0c3d4c5e66754df945e00fa0fa6
KatliKoma <katlegokoma@1gmail.com>
2023-10-19 20:33:06 +0200

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,16 @@
+#include <sdtio.h>
+/**
+ * _strncpy - copies a string.
+ * @dest: is the initial string
+ * @src: the string which appends to the initial string
+ * @n: count on index
+ * Return: the string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 
-	while (*src && n > 0) 
+	while (*src && n > 0)
 	{
 
 		*dest = *src;
@@ -11,13 +19,12 @@ char *_strncpy(char *dest, char *src, int n)
 		n--;
 	}
 
-   
 	while (n > 0)
-       	{
+	{
 		*dest = '\0';
 		dest++;
 		n--;
 	}
 
-	return result;
+	return (result);
 }
