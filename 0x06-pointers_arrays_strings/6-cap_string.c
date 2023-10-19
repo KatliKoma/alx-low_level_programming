281e105aa549594386a96c669661823ceffb1625
KatliKoma <katlegokoma@1gmail.com>
2023-10-20 01:10:48 +0200

    capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,29 +7,43 @@
  *
  * Return: the string
  */
-char *cap_string(char *str)
+#include "main.h"
+
+char *cap_string(char *str) 
 {
-	int capitalize_next = 1;
+	int index = 0;
+	int cap_next = 1;
 
-	for (int i = 0; str[i] != '\0'; i++)
+	while (str[index])
 	{
-		if (isspace(str[i]) || str[i] == '\t' || str[i] == '\n' ||
-				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-			capitalize_next = 1;
-		}
-		else if (capitalize_next)
-		{
-			str[i] = toupper(str[i]);
-			capitalize_next = 0;
-		}
-		else
-		{
-			str[i] = tolower(str[i]);
-		}
-	}
+		char c = str[index];
 
-	return (str);
+		switch (c) 
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				cap_next = 1;
+				break;
+			default:
+				if (cap_next && c >= 'a' && c <= 'z')
+				{
+					str[index] -= 32;
+					cap_next = 0;
+				}
+		}
+		index++;
+	}
+	return str;
 }
+
