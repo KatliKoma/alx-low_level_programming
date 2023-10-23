e8c3197c4569fa2f63482c4c6c34464b3db89320
KatliKoma <katlegokoma@1gmail.com>
2023-10-23 21:56:58 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,11 @@
+#include "main.h"
 /**
  * _strspn -gets the length of a prefix substring.
- * @s: The null-terminated string to be examined.
- * @accept:The string containing characters to match in the prefix of 's'.
- * Return:number of bytes in the initial segment of 's'.
+ * @s:
+ * @accept:
+ * 
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
-	int is_matching = 1;
-
-	while (*s && is_matching)
-	{
-		is_matching = 0;
-		for (char *a = accept; *a; a++)
-		{
-			if (*s == *a)
-			{
-				is_matching = 1;
-				break;
-			}
-		}
-
-		if (is_matching)
-		{
-			count++;
-			s++;
-		}
-	}
-
-	return (count);
+	return (_strspn(s, accept));
 }
-
