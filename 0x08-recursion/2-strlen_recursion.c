9137daf5225386dcfaf007e7bc343fe7d6e21025
KatliKoma <katlegokoma@1gmail.com>
2023-10-25 13:14:37 +0200

    lets count

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,14 @@
-#include "main.h"
 /**
  * _strlen_recursion - length of a string.
- *@s:string to be counted
- *return: returns int length.
+ * @s: string to be counted
+ * Return: returns int length.
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-		
-	return 1+ _strlen_recursion(s+1);
+    if (*s == '\0')
+    {
+	return 0;
+    }
+    return 1 + _strlen_recursion(s + 1);
 }
+
