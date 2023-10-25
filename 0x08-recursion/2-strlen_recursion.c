ded902dd7582ea4745f6214babff420034aaf1d0
KatliKoma <katlegokoma@1gmail.com>
2023-10-25 12:40:23 +0200

    lets count

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen_recursion - length of a string.
+ *@s:string to be counted
+ *return: returns int length.
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		i++;
+		_sterlen_recursion(*s);
+	}
+	return (length);
+}
