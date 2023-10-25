cc2c70f80f2388ec11a49099167b64f1f6557b80
KatliKoma <katlegokoma@1gmail.com>
2023-10-25 23:52:57 +0200

    strings can be considered identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * wildcmp - Compares two strings with wildcard support.
  * @s1: The first string to compare.
@@ -7,11 +9,15 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && wildcmp(s1, s2 + 1))))
+	if (*s1 == '\0')
+	{
+		if (*s2 == '\0' || (*s2 == '*' && wildcmp(s1, s2 + 1)))
 		return (1);
+	return (0);
+	}
 
 	if (*s2 == '*')
-		return ((wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
