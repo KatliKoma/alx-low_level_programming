e7eacbffc23ae5c3633f7e968bdd10598129ccb6
KatliKoma <katlegokoma@1gmail.com>
2023-10-25 23:15:01 +0200

    prime numbers

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,30 +1,13 @@
 /**
- * is_palindrome - Check if a string is a palindrome.
- * @s: The input string to check.
- *
- * Return: 1 if s is a palindrome, 0 otherwise.
- */
-int is_palindrome(char *s)
-{
-	int length;
-
-	length = find_string_length(s);
-
-	if (length == 0)
-		return (1);
-	return (check_palindrome(s, 0, length - 1));
-}
-
-/**
- * find_string_length - Calculate the length of a string.
+ * lengthc - Calculate the length of a string.
  * @s: The input string to find the length of.
  *
  * Return: The length of the string.
  */
-int find_string_length(char *s)
+int lengthc(char *s)
 {
 	if (*s != '\0')
-		return (1 + find_string_length(s + 1));
+		return (1 + lengthc(s + 1));
 	return (0);
 }
 
@@ -43,7 +26,24 @@ int check_palindrome(char str[], int start, int end)
 	if (str[start] != str[end])
 		return (0);
 	if (start <= end || start < end + 1)
-	return (check_palindrome(str, start + 1, end - 1));
+		return (check_palindrome(str, start + 1, end - 1));
 	return (1);
 }
 
+
+/**
+ * is_palindrome - Check if a string is a palindrome.
+ * @s: The input string to check.
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int length;
+
+	length = lengthc(s);
+
+	if (length == 0)
+		return (1);
+	return (check_palindrome(s, 0, length - 1));
+}
