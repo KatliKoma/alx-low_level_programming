cd0e59b8506940541e22d5b7b1d225881c039cb3
KatliKoma <katlegokoma@1gmail.com>
2023-10-31 15:01:39 +0200

    Float like a butterfly

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * custom_create_array - array of chars, and initializes it with a  char.
+ * @size: the size of the array to create
+ * @c: the character to initialize the array with.
+ * Return: NULL if size is 0 or if it fails to allocate memory.
+ */
+
+char *custom_create_array(unsigned int size, char c)
+{
+	char *newArray = NULL;
+	unsigned int index;
+
+	if (size > 0)
+	{
+		newArray = (char *)malloc(size * sizeof(char));
+		if (newArray == NULL)
+		{
+			return (NULL);
+		}
+
+		for (index = 0; index < size; index++)
+		{
+			newArray[index] = c;
+		}
+	}
+	return (newArray);
+}
