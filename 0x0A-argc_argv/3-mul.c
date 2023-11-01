924797a378ce6deed1f88772539e2ac983db8051
KatliKoma <katlegokoma@1gmail.com>
2023-11-01 19:39:51 +0200

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers and prints the result
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 for success, 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int firstNum = atoi(argv[1]);
+
+	int secondNum = atoi(argv[2]);
+	int result = firstNum * secondNum;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
