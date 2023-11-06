d15773925ef3548739c5a872f060bc84643ce3a5
KatliKoma <katlegokoma@1gmail.com>
2023-11-06 21:07:54 +0200

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
