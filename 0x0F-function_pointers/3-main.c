9d073d49e42bc4b187ae81a148052685ab3ef513
KatliKoma <katlegokoma@1gmail.com>
2023-11-08 22:17:38 +0200

    A goal is not always meant to be reached,

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,26 +2,21 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-/**
- * main - The main function of the simple calculator program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Return: 0 if the program executed successfully, 98 if the number of
- */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+	char *operator;
+	int (*calc_func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	int num1 = atoi(argv[1]);
-	char *operator = argv[2];
-	int num2 = atoi(argv[3]);
-
-	int (*calc_func)(int, int);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
 
 	calc_func = get_op_func(operator);
 
@@ -31,9 +26,9 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 
-	int result = calc_func(num1, num2);
-
+	result = calc_func(num1, num2);
 	printf("%d\n", result);
 
 	return (0);
 }
+
