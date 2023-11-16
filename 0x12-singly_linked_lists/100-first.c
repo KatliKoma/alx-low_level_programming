e7927227ac3e253e6ce883f380a9f7d05b3287fc
KatliKoma <katlegokoma1@gmail.com>
2023-11-16 22:30:50 +0200

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,6 @@
 /**
  * before_main - Function to be executed before main.
  */
-
-void before_main(void) __attribute__((constructor));
-
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
