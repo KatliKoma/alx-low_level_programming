56d09c7e6bc6a7ac439664d32bf7070ca260931a
KatliKoma <katlegokoma1@gmail.com>
2023-11-16 21:04:02 +0200

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#inlcude "lists.h"
+
+/**
+ * list_len - number of elements in a linked list_t list
+ * @h: A pointer to the head of the list.
+ *
+ * Return: count of elements in the linked list.
+ */
+size_t list_len(const list_t *h)
+{
+
+	siz_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
