adfc942c123a52df64c6982d45bb611846c60d21
KatliKoma <katlegokoma1@gmail.com>
2023-11-21 21:18:52 +0200

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 
@@ -24,4 +24,3 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	return (new_node);
 }
-
