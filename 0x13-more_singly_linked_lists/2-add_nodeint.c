131772d68ce3cf4943d5d0f6dc9259541d9a4f43
KatliKoma <katlegokoma1@gmail.com>
2023-11-21 21:17:32 +0200

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,38 +1,27 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
-* add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to the head.
- * @str: The string to be duplicated and added.
- *
- * Return: The address of the new element.
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: A pointer to the pointer to the head of the list.
+ * @n: The value to set in the new node.
+ * Return: The address of the new element, or NULL if it failed.
  */
-
-list_t *add_node(list_t **head, const char *str)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *new_node;
+	listint_t *new_node;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
-		return (NULL);
+		return NULL;
 
-	new_node->str = strdup(str);
+	new_node->n = n;
 
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = strlen(str);
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
 }
+
