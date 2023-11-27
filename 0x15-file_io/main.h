57fbb0e7af4eb7354765e2c45348acac24a9fc86
KatliKoma <katlegokoma1@gmail.com>
2023-11-27 22:45:13 +0200

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char *source, const char *destination);
-void copy_file(const char *source_file, const char *destination_file)i;
+void print_error_and_exit(int code, const char *message, const char *file_name);
 
 #endif
