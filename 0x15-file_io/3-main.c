2804336b22594314c189648e8a4b3c04e63d358a
KatliKoma <katlegokoma1@gmail.com>
2023-11-27 22:43:35 +0200

    cp

---
+++ 0x15-file_io/3-main.c
@@ -0,0 +1,12 @@
+int main(int argc, char *argv[])
+{
+
+	if (argc != 3)
+	{
+		print_error_and_exit(97, "Usage: %s file_from file_to\n", argv[0]);
+	}
+
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}
