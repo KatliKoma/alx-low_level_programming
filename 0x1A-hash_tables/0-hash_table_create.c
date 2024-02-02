9092323dbdb972d02a8c00d44a0ef6679ba6c03c
KatliKoma <katlegokoma1@gmail.com>
2024-02-02 20:42:17 +0200

    ht

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: Pointer to newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (!hash_table)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = calloc((size_t)hash_table->size, sizeof(hash_node_t *));
+
+	if (hash_table->array == NULL)
+		return (NULL);
+
+
+	return (hash_table);
+}
